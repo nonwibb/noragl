0fcad597e71ff7e989f1dc2b5cec9622c8f94498
nonwibb <45381459+nonwibb@users.noreply.github.com>
2022-10-30 17:29:59 -0400

    remove build to readd

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nonwibb/Documents/noragl
-# Build directory: /home/nonwibb/Documents/noragl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
